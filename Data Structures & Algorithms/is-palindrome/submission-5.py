0c87013e4bdce1069f844d0db57f893dfcb5ee2c
Samyog Maharjan <msamyog37@gmail.com>
2026-04-20 18:34:56 -0500

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Two Pointer Technique
+        l=0
+        r=len(s)-1
+
+        s=s.lower()
+        while l<r:
+            while not s[l].isalnum() and l<r:
+                l+=1
+            while not s[r].isalnum() and r>l:
+                r-=1
+            
+            if s[l]!=s[r]:
+                return False
+            l,r=l+1,r-1
+        
+        return True 
+
