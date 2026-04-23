bf7015bc5060fcd2b58b851bc1351bfe8e462fb7
Samyog Maharjan <msamyog37@gmail.com>
2026-04-22 21:20:36 -0500

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # #0(1)-Space Complexity 
+        # if len(s)!=len(t):
+        #     return False
+        
+        # countS,countT={},{}
+
+        # for i in range(len(s)):
+        #     countS[s[i]]=1+countS.get(s[i],0)
+        #     countT[t[i]]=1+countT.get(t[i],0)
+
+        # for c in countT:
+        #     if countT[c]!=countS.get(c,0):
+        #         return False
+            
+        # return True 
+
+        if len(s)!=len(t):
+            return False
+        
+        s_count=[0]*26
+        t_count=[0]*26
+
+        for i in range(len(s)):
+            s_count[ord(s[i])-ord('a')]+=1
+        for i in range(len(s)):
+            t_count[ord(t[i])-ord('a')]+=1
+        
+        return s_count==t_count
+        
