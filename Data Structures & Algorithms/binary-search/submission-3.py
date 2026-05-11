58c1b354cd0dc371c274e2aa628f6735b152dc23
Samyog Maharjan <msamyog37@gmail.com>
2026-05-10 21:41:54 -0500

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        #linear search
+
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        
+        return -1
+
+            
+
+
+
+
