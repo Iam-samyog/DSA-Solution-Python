5032736dc71c779c61542cea91186b54c8504a9c
Samyog Maharjan <msamyog37@gmail.com>
2026-05-21 20:29:53 -0500

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        maxP=0
+        for i in range(len(prices)):
+            for j in range(i+1,len(prices)):
+                profit=prices[j]-prices[i]
+                maxP=max(profit,maxP)
+        
+        return maxP
+        
