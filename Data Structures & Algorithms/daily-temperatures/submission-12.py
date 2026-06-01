f2921304e6f127cde20938c44bd11c3861da13b0
Samyog Maharjan <msamyog37@gmail.com>
2026-06-01 18:35:49 -0500

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.py
@@ -0,0 +1,29 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        #Brute Force Approach 
+        result=[]
+        for i in range(len(temperatures)):
+            found=False
+            for j in range(i+1,len(temperatures)):
+                if temperatures[j]>temperatures[i]:
+                    result.append(j-i)
+                    found=True
+                    break
+
+            if not found:
+                result.append(0)
+            
+            
+        
+        return result
+                
+
+
+
+        
+
+
+            
+
+                    
